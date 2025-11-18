a3ad91ede1564fb4c616cd9e3fdba3aba7206237
Shubham Verma <75219986+Shverma3696@users.noreply.github.com>
2025-11-18 23:24:24 +0000

    practice - how to print in java

---
+++ 01-Basics/1.1-How-To-Print/HowToPrint.java
@@ -0,0 +1,9 @@
+
+public class HowToPrint {
+    public static void main(String[] args) {
+        System.out.println("hello world"); // style 1
+        System.out.print("hello world\n"); // style 2
+
+        System.out.print("*\n**\n***\n");
+    }
+}
---
+++ 01-Basics/1.2-PrintZ/PrintZ.java
@@ -0,0 +1,16 @@
+/**
+ * Patter to print
+ * 
+ * x x x x x
+ * - - - x
+ * - - x
+ * - x
+ * x x x x x
+ * 
+ */
+
+public class PrintZ {
+    public static void main(String[] args) {
+        System.out.print("xxxxx\n   x\n  x\n x\nxxxxx");
+    }
+}
