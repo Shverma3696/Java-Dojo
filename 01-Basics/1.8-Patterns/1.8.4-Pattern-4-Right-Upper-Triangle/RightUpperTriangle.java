8a72d457a8e4caa22cfb70cebe0b733951ecc544
Shubham Verma <75219986+Shverma3696@users.noreply.github.com>
2025-11-30 22:16:36 +0000

    right upper triangle

---
+++ 01-Basics/1.8-Patterns/1.8.4-Pattern-4-Right-Upper-Triangle/RightUpperTriangle.java
@@ -0,0 +1,38 @@
+/**
+ * 1. given a number 'n'.
+ * 
+ * Print pattern (E.g., n = 5):
+ * [*****]
+ * [-****]
+ * [--***]
+ * [---**]
+ * [----*]
+ * 
+ */
+
+public class RightUpperTriangle {
+    public static void main(String[] args) {
+
+        int n = 5;
+
+        int st = n; // number of stars
+        int sp = 0; // number of spaces
+
+        for (int i = 1; i <= n; i++) { // control rows
+
+            for (int j = 1; j <= sp; j++) { // control columns for SPACES to be printed first
+                System.out.print("\s\s"); // print (use \s : for space or \t : for tab)
+                // using doble spaces to even out the spaces in the final output pattern
+            }
+
+            for (int j = 1; j <= st; j++) { // control columns for STARS
+                System.out.print("*\s"); // print (use \s : for space or \t : for tab)
+            }
+
+            sp++; // spaces keeps decreasing by 1 as we move to next row
+            st--; // stars keeps increasing by 1 as we move to next row
+
+            System.out.println(); // end line or change line
+        }
+    }
+}
