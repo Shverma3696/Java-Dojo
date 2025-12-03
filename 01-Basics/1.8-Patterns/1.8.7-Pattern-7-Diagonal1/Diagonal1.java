92a351dcb69ab6bc781e1ed05255f1b872f2fa95
Shubham Verma <75219986+Shverma3696@users.noreply.github.com>
2025-12-03 18:34:04 +0000

    approach 3 added

--- 01-Basics/1.8-Patterns/1.8.7-Pattern-7-Diagonal1/Diagonal1.java
+++ 01-Basics/1.8-Patterns/1.8.7-Pattern-7-Diagonal1/Diagonal1.java
@@ -15,33 +15,49 @@ public class Diagonal1 {
 
         int n = 5;
 
-        int st = 1; // number of stars is always 1
-        int sp = 0; // number of spaces
-
-        // approach 1
-
-        for (int i = 1; i <= n; i++) { // control rows
-
-            for (int j = 1; j <= sp; j++) { // control columns for SPACES to be printed first
-                System.out.print("\s\s"); // print (use \s : for space or \t : for tab)
-                // using doble spaces to even out the spaces in the final output pattern
+        // approach 3
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i == j) {
+                    System.out.print("*\s"); // print (use \s : for space or \t : for tab)
+                } else {
+                    System.out.print("\s\s");
+                }
             }
-
-            for (int j = 1; j <= st; j++) { // control columns for STARS
-                System.out.print("*\s"); // print (use \s : for space or \t : for tab)
-            }
-
-            // since, main items of line 1 is already printed
-            // below is the prep for next line
-
-            sp++;
-            System.out.println(); // end line or change line
+            System.out.println(); // end line
         }
 
     }
 }
 
 /**
+ * 
+ * 
+ * // int st = 1; // number of stars is always 1
+ * // int sp = 0; // number of spaces
+ * 
+ * // approach 1
+ * 
+ * // for (int i = 1; i <= n; i++) { // control rows
+ * 
+ * // for (int j = 1; j <= sp; j++) { // control columns for SPACES to be
+ * printed
+ * // first
+ * // System.out.print("\s\s"); // print (use \s : for space or \t : for tab)
+ * // // using doble spaces to even out the spaces in the final output pattern
+ * // }
+ * 
+ * // for (int j = 1; j <= st; j++) { // control columns for STARS
+ * // System.out.print("*\s"); // print (use \s : for space or \t : for tab)
+ * // }
+ * 
+ * // // since, main items of line 1 is already printed
+ * // // below is the prep for next line
+ * 
+ * // sp++;
+ * // System.out.println(); // end line or change line
+ * // }
+ * 
  * // approach 2
  * for (int i = 1; i <= n; i++) {
  * for (int j = 1; j <= i; j++) {
@@ -54,4 +70,5 @@ public class Diagonal1 {
  * sp++;
  * System.out.println(); // end line
  * }
+ * 
  */
