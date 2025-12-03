7391e2b7c3bf105e4b22aa9b5eedbc175951cb91
Shubham Verma <75219986+Shverma3696@users.noreply.github.com>
2025-12-03 17:51:55 +0000

    diagonal 1 from left upper corner to right bottom corner

---
+++ 01-Basics/1.8-Patterns/1.8.7-Pattern-7-Diagonal1/HollowDiamond.java
@@ -0,0 +1,51 @@
+/**
+ * 1. given a number 'n'.
+ * 
+ * Print pattern (E.g., n = 5):
+ * [***-***]
+ * [**---**]
+ * [*-----*]
+ * [**---**]
+ * [***-***]
+ * 
+ */
+
+public class HollowDiamond {
+    public static void main(String[] args) {
+
+        int n = 5;
+
+        int st = n / 2 + 1; // number of stars
+        int sp = 1; // number of spaces
+
+        for (int i = 1; i <= n; i++) { // control rows
+
+            for (int j = 1; j <= st; j++) { // control columns for STARS
+                System.out.print("*\s"); // print (use \s : for space or \t : for tab)
+            }
+
+            for (int j = 1; j <= sp; j++) { // control columns for SPACES to be printed first
+                System.out.print("\s\s"); // print (use \s : for space or \t : for tab)
+                // using doble spaces to even out the spaces in the final output pattern
+            }
+
+            for (int j = 1; j <= st; j++) { // control columns for STARS
+                System.out.print("*\s"); // print (use \s : for space or \t : for tab)
+            }
+
+            // since, main items of line 1 is already printed
+            // below is the prep for next line
+            // controlling the values of stars and spaces for next line to be printed
+
+            if (i <= n / 2) {
+                st--; // stars keeps decreasing by 1 as we move to next row, till mid row
+                sp += 2; // spaces keeps increasing by 2 as we move to next row, till mid row
+            } else {
+                st++; // stars keeps increasing by 1 as we move past mid row
+                sp -= 2; // spaces keeps decreasing by 2 as we move past mid row
+            }
+
+            System.out.println(); // end line or change line
+        }
+    }
+}
