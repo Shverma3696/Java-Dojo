3312776ae4d30c63d6c6a4d9f73d0f3d2b5f0a6b
Shubham Verma <75219986+Shverma3696@users.noreply.github.com>
2025-12-03 17:58:39 +0000

    code modified and updated

--- 01-Basics/1.8-Patterns/1.8.7-Pattern-7-Diagonal1/HollowDiamond.java
+++ 01-Basics/1.8-Patterns/1.8.7-Pattern-7-Diagonal1/HollowDiamond.java
@@ -2,11 +2,11 @@
  * 1. given a number 'n'.
  * 
  * Print pattern (E.g., n = 5):
- * [***-***]
- * [**---**]
- * [*-----*]
- * [**---**]
- * [***-***]
+ * [*----]
+ * [-*---]
+ * [--*--]
+ * [---*-]
+ * [----*]
  * 
  */
 
@@ -15,14 +15,10 @@ public class HollowDiamond {
 
         int n = 5;
 
-        int st = n / 2 + 1; // number of stars
-        int sp = 1; // number of spaces
+        int st = 1; // number of stars is always 1
+        int sp = 0; // number of spaces
 
         for (int i = 1; i <= n; i++) { // control rows
-
-            for (int j = 1; j <= st; j++) { // control columns for STARS
-                System.out.print("*\s"); // print (use \s : for space or \t : for tab)
-            }
 
             for (int j = 1; j <= sp; j++) { // control columns for SPACES to be printed first
                 System.out.print("\s\s"); // print (use \s : for space or \t : for tab)
@@ -37,14 +33,15 @@ public class HollowDiamond {
             // below is the prep for next line
             // controlling the values of stars and spaces for next line to be printed
 
-            if (i <= n / 2) {
-                st--; // stars keeps decreasing by 1 as we move to next row, till mid row
-                sp += 2; // spaces keeps increasing by 2 as we move to next row, till mid row
-            } else {
-                st++; // stars keeps increasing by 1 as we move past mid row
-                sp -= 2; // spaces keeps decreasing by 2 as we move past mid row
-            }
+            // if (i <= n / 2) {
+            // st--; // stars keeps decreasing by 1 as we move to next row, till mid row
+            // sp += 2; // spaces keeps increasing by 2 as we move to next row, till mid row
+            // } else {
+            // st++; // stars keeps increasing by 1 as we move past mid row
+            // sp -= 2; // spaces keeps decreasing by 2 as we move past mid row
+            // }
 
+            sp++;
             System.out.println(); // end line or change line
         }
     }
