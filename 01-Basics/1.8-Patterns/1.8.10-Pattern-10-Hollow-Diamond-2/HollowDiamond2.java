7445075a40beb570af5689f636681c2f831d3c08
Shubham Verma <75219986+Shverma3696@users.noreply.github.com>
2025-12-05 12:29:58 +0530

    pattern 10 - hollow diamond 2

---
+++ 01-Basics/1.8-Patterns/1.8.10-Pattern-10-Hollow-Diamond-2/HollowDiamond2.java
@@ -0,0 +1,21 @@
+/**
+ * 1. given a number 'n'.
+ * 
+ * Print pattern (E.g., n = 5):
+ * [--*--]
+ * [-*-*-]
+ * [*---*]
+ * [-*-*-]
+ * [--*--]
+ * 
+ */
+
+public class HollowDiamond2 {
+    public static void main(String[] args) {
+
+        int n = 5;
+
+        // approach 1
+
+    }
+}
