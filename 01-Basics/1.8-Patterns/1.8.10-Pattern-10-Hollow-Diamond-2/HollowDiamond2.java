f21d58a430ae8329b23eadbd185f4ecbda937b96
Shubham Verma <75219986+Shverma3696@users.noreply.github.com>
2025-12-05 13:03:12 +0530

    approach 2 - hollow diamond 2

--- 01-Basics/1.8-Patterns/1.8.10-Pattern-10-Hollow-Diamond-2/HollowDiamond2.java
+++ 01-Basics/1.8-Patterns/1.8.10-Pattern-10-Hollow-Diamond-2/HollowDiamond2.java
@@ -15,12 +15,9 @@ public class HollowDiamond2 {
 
         int n = 5;
 
-        // approach 1
+        // approach 2
         int osp = n / 2; // outer spaces
         int isp = -1; // inner spaces
-
-        int st1 = 1; // star for first and last rows, always 1
-        int st2 = 2; // stars for middle rows, always 2
 
         for (int i = 1; i <= n; i++) {
 
@@ -29,17 +26,15 @@ public class HollowDiamond2 {
                 System.out.print("\s\s");
             }
 
-            if (i == 1 || i == n) {
-                System.out.print("*\s"); // print (use \s : for space or \t : for tab)
-            } else {
-                System.out.print("*\s");
+            System.out.print("*\s"); // print (use \s : for space or \t : for tab)
 
-                // print iner spaces
-                for (int j = 1; j <= isp; j++) {
-                    System.out.print("\s\s");
-                }
+            // print inner spaces
+            for (int j = 1; j <= isp; j++) {
+                System.out.print("\s\s");
+            }
 
-                System.out.print("*\s");
+            if (i > 1 && i < n) {
+                System.out.print("*\s"); // second star
             }
 
             if (i <= n / 2) {
@@ -55,3 +50,40 @@ public class HollowDiamond2 {
 
     }
 }
+
+/**
+ * // approach 1
+ * int osp = n / 2; // outer spaces
+ * int isp = -1; // inner spaces
+ * 
+ * for (int i = 1; i <= n; i++) {
+ * 
+ * // print outer spaces
+ * for (int j = 1; j <= osp; j++) {
+ * System.out.print("\s\s");
+ * }
+ * 
+ * if (i == 1 || i == n) {
+ * System.out.print("*\s"); // print (use \s : for space or \t : for tab)
+ * } else {
+ * System.out.print("*\s");
+ * 
+ * // print inner spaces
+ * for (int j = 1; j <= isp; j++) {
+ * System.out.print("\s\s");
+ * }
+ * 
+ * System.out.print("*\s");
+ * }
+ * 
+ * if (i <= n / 2) {
+ * osp--;
+ * isp += 2;
+ * } else {
+ * osp++;
+ * isp -= 2;
+ * }
+ * 
+ * System.out.println(); // end line
+ * }
+ */
