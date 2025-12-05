512ede52e1f90f66894d05e25a18e003420c5155
Shubham Verma <75219986+Shverma3696@users.noreply.github.com>
2025-12-05 12:21:51 +0530

    pattern 9 - both diagonals

---
+++ 01-Basics/1.8-Patterns/1.8.9-Pattern-9-Both-Diagonals/BothDiagonals.java
@@ -0,0 +1,25 @@
+
+public class BothDiagonals {
+    public static void main(String[] args) {
+
+        int n = 5;
+
+        // approach 1
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i == j || i + j == n + 1) {
+                    System.out.print("*\s"); // print (use \s : for space or \t : for tab)
+                } else {
+                    System.out.print("\s\s");
+                }
+            }
+            System.out.println(); // end line
+        }
+
+    }
+}
+
+/**
+ * 
+ * 
+ */
