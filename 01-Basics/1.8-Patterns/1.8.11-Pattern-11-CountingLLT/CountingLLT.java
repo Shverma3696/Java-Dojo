0f8a4b21600d70010f1a589d76a88d1ab9dc3fdf
Shubham Verma <75219986+Shverma3696@users.noreply.github.com>
2025-12-06 12:29:08 +0530

    comments added

--- 01-Basics/1.8-Patterns/1.8.11-Pattern-11-CountingLLT/CountingLLT.java
+++ 01-Basics/1.8-Patterns/1.8.11-Pattern-11-CountingLLT/CountingLLT.java
@@ -2,11 +2,11 @@
  * 1. given a number 'n'.
  * 
  * Print pattern (E.g., n = 5):
- * [--*--]
- * [-*-*-]
- * [*---*]
- * [-*-*-]
- * [--*--]
+ * [1]
+ * [2-3]
+ * [4-5-6]
+ * [7-8-9-10]
+ * [11-12-13-14-15]
  * 
  */
 
@@ -14,6 +14,10 @@ public class CountingLLT {
     public static void main(String[] args) {
 
         int n = 5;
+
+        for (int i = 1; i <= n; i++) {
+
+        }
 
     }
 }
