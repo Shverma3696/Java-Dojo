89db9531cbf036689516947fca5759f874f15ab3
Shubham Verma <75219986+Shverma3696@users.noreply.github.com>
2025-12-06 12:27:26 +0530

    pattern 11 - counting left lower triangle

---
+++ 01-Basics/1.8-Patterns/1.8.11-Pattern-11-CountingLLT/CountingLLT.java
@@ -0,0 +1,23 @@
+/**
+ * 1. given a number 'n'.
+ * 
+ * Print pattern (E.g., n = 5):
+ * [--*--]
+ * [-*-*-]
+ * [*---*]
+ * [-*-*-]
+ * [--*--]
+ * 
+ */
+
+public class CountingLLT {
+    public static void main(String[] args) {
+
+        int n = 5;
+
+    }
+}
+
+/**
+ * // approach 1
+ */
