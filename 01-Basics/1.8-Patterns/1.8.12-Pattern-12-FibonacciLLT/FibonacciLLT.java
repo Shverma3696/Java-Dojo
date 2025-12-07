042402d3d437f6b2168197c86d6d1ee246b62bad
Shubham Verma <75219986+Shverma3696@users.noreply.github.com>
2025-12-07 14:21:30 +0530

    code running as expected

--- 01-Basics/1.8-Patterns/1.8.12-Pattern-12-FibonacciLLT/FibonacciLLT.java
+++ 01-Basics/1.8-Patterns/1.8.12-Pattern-12-FibonacciLLT/FibonacciLLT.java
@@ -16,11 +16,15 @@ public class FibonacciLLT {
 
         // approach 1
 
-        int count = 1;
+        int a = 0;
+        int b = 1;
+
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= i; j++) {
-                // System.out.print(count + "\t");
-                // count++;
+                System.out.print(a + "\t");
+                int c = a + b;
+                a = b;
+                b = c;
             }
             System.out.println();
         }
