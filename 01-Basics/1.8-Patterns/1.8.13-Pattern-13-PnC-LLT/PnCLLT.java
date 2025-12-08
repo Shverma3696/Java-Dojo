8a744bf341687893f99a9ce6c9bfee6183c44dc6
Shubham Verma <75219986+Shverma3696@users.noreply.github.com>
2025-12-08 10:58:54 +0530

    code running

--- 01-Basics/1.8-Patterns/1.8.13-Pattern-13-PnC-LLT/PnCLLT.java
+++ 01-Basics/1.8-Patterns/1.8.13-Pattern-13-PnC-LLT/PnCLLT.java
@@ -1,7 +1,7 @@
 /**
  * 1. given a number 'n'. (number of rows)
  * 
- * Print pattern (E.g., n = 4):
+ * Print pattern (E.g., n = 6):
  * [1]
  * [1-1]
  * [1-2-1]
@@ -9,17 +9,28 @@
  * [1-4-6-4-1]
  * [1-5-10-10-5-1]
  * 
+ * Hint : this pattern is based on combinations formular
+ * => nCr = n! / r! * (n-r)!
+ * 
+ * Actual formula used for code:
+ * => nC(k+1) = nCk * (n-k) / (k+1)
+ * 
+ * where, n = row number (i); k = column number (j)
+ * 
  */
 
 public class PnCLLT {
     public static void main(String[] args) {
 
-        int n = 5;
+        int n = 6;
 
         // approach 1
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= i; j++) {
-                System.out.print("*\t");
+        for (int i = 0; i < n; i++) {
+            int val = 1;
+
+            for (int j = 0; j <= i; j++) {
+                System.out.print(val + "\t");
+                val = val * (i - j) / (j + 1); // val = nCk
             }
             System.out.println();
         }
