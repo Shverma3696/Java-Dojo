3f27c41ff6bb9007ca40dfd50c9ffcf7aedc670f
Shubham Verma <75219986+Shverma3696@users.noreply.github.com>
2025-12-10 09:06:11 +0530

    code running

---
+++ 01-Basics/1.8-Patterns/1.8.15-Pattern-15-Number-Diamond/NumberDiaomond.java
@@ -0,0 +1,55 @@
+/**
+ * 1. given a number 'n'. (number of rows)
+ * 
+ * Print pattern (E.g., n = 5):
+ * [--1--]
+ * [-232-]
+ * [34543]
+ * [-232-]
+ * [--1--]
+ * 
+ */
+
+public class NumberDiaomond {
+    public static void main(String[] args) {
+
+        int n = 5;
+
+        // approach 1
+        int sp = n / 2; // number of spaces
+        int st = 1; // number of stars
+
+        int val = 1; // to print
+        for (int i = 1; i <= n; i++) {
+
+            // print spaces first
+            for (int j = 1; j <= sp; j++) {
+                System.out.print("\t");
+            }
+
+            // print stars
+            int cval = val; // computed value to be printed
+            for (int j = 1; j <= st; j++) {
+                System.out.print(cval + "\t");
+
+                if (j <= st / 2) {
+                    cval++;
+                } else {
+                    cval--;
+                }
+            }
+
+            if (i <= n / 2) {
+                sp--;
+                st += 2;
+                val++;
+            } else {
+                sp++;
+                st -= 2;
+                val--;
+            }
+            System.out.println();
+        }
+
+    }
+}
