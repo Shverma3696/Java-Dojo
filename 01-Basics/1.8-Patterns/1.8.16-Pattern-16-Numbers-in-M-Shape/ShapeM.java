84cc2475b4d9665cbe2056c11d40bded716737a3
Shubham Verma <75219986+Shverma3696@users.noreply.github.com>
2025-12-13 19:32:00 +0530

    star pattern created and running

--- 01-Basics/1.8-Patterns/1.8.16-Pattern-16-Numbers-in-M-Shape/ShapeM.java
+++ 01-Basics/1.8-Patterns/1.8.16-Pattern-16-Numbers-in-M-Shape/ShapeM.java
@@ -14,6 +14,38 @@ public class ShapeM {
 
         int n = 4;
 
+        // approach 1
+        int col = (2 * n) - 1;
+
+        int st = 1;
+        int sp = (2 * n) - 3;
+
+        for (int i = 1; i <= n; i++) {
+
+            if (i == n) {
+                for (int j = 1; j <= col; j++) {
+                    System.out.print("*\t");
+                }
+                break;
+            }
+
+            for (int j = 1; j <= st; j++) {
+                System.out.print("*\t");
+            }
+
+            for (int j = 1; j <= sp; j++) {
+                System.out.print("\t");
+            }
+
+            for (int j = 1; j <= st; j++) {
+                System.out.print("*\t");
+            }
+
+            st++;
+            sp -= 2;
+            System.out.println();
+        }
+
     }
 }
 
