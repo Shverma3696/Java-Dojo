c848dc321f5f3cb99ac99347d3c23db56a932371
Shubham Verma <75219986+Shverma3696@users.noreply.github.com>
2025-12-13 19:24:34 +0530

    pattern 16 - number pattern in shape that kind of looks like M

---
+++ 01-Basics/1.8-Patterns/1.8.16-Pattern-16-Numbers-in-M-Shape/ShapeM.java
@@ -0,0 +1,20 @@
+/**
+ * 1. given a number 'n'. (number of rows)
+ * 
+ * Print pattern (E.g., n = 4):
+ * [1-----1]
+ * [12---21]
+ * [123-321]
+ * [1234321]
+ * 
+ */
+
+public class ShapeM {
+    public static void main(String[] args) {
+
+        int n = 4;
+
+    }
+}
+
+// try again for better understanding
