32fc10298c4bb0a165b432ae2869d72c80b24618
Shubham Verma <75219986+Shverma3696@users.noreply.github.com>
2025-12-14 12:02:35 +0530

    pattern 17 - arrow shape with stars

---
+++ 01-Basics/1.8-Patterns/1.8.17-Pattern-17-Arrow/ArrowShape.java
@@ -0,0 +1,21 @@
+/**
+ * 1. given a number 'n'. (always odd)
+ * 
+ * Print pattern (E.g., n = 5):
+ * [--*--]
+ * [--**-]
+ * [*****]
+ * [--**-]
+ * [--*--]
+ * 
+ */
+
+public class ArrowShape {
+    public static void main(String[] args) {
+
+        int n = 5;
+
+        // approach 1
+
+    }
+}
