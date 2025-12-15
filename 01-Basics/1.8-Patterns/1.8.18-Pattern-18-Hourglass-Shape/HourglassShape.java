ca2e93d2f0004b8c94396a0a23316dfc681b8d55
Shubham Verma <75219986+Shverma3696@users.noreply.github.com>
2025-12-15 11:07:29 +0530

    full hourglass running

--- 01-Basics/1.8-Patterns/1.8.18-Pattern-18-Hourglass-Shape/HourglassShape.java
+++ 01-Basics/1.8-Patterns/1.8.18-Pattern-18-Hourglass-Shape/HourglassShape.java
@@ -18,6 +18,30 @@ public class HourglassShape {
         int n = 7;
 
         // approach 1
+        int st = n;
+        int sp = 0;
+
+        for (int i = 1; i <= n; i++) {
+
+            // spaces
+            for (int j = 1; j <= sp; j++) {
+                System.out.print("\t");
+            }
+
+            // stars
+            for (int j = 1; j <= st; j++) {
+                System.out.print("*\t");
+            }
+
+            if (i <= n / 2) {
+                sp++;
+                st -= 2;
+            } else {
+                sp--;
+                st += 2;
+            }
+            System.out.println();
+        }
 
     }
 }
