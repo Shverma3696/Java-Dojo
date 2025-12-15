bd2c013125a4328442c10c5f8137ecdf3220ed43
Shubham Verma <75219986+Shverma3696@users.noreply.github.com>
2025-12-15 11:01:11 +0530

    pattern 18 - hourglass shape

---
+++ 01-Basics/1.8-Patterns/1.8.18-Pattern-18-Hourglass-Shape/HourglassShape.java
@@ -0,0 +1,23 @@
+/**
+ * 1. given a number 'n' = 7. (always odd)
+ * 
+ * Print pattern (E.g., n = 7):
+ * [*******]
+ * [-*---*-]
+ * [--*-*--]
+ * [---*---]
+ * [--***--]
+ * [-*****-]
+ * [*******]
+ * 
+ */
+
+public class HourglassShape {
+    public static void main(String[] args) {
+
+        int n = 7;
+
+        // approach 1
+
+    }
+}
