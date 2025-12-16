6b65f57485a8b668fb325bdd71e2293f4a15d8dd
Shubham Verma <75219986+Shverma3696@users.noreply.github.com>
2025-12-17 00:38:05 +0530

    pattern 20 - w shape

---
+++ 01-Basics/1.8-Patterns/1.8.20-Pattern-20-W-Shape/WShape.java
@@ -0,0 +1,22 @@
+/**
+ * 1. given a number 'n'. (always odd)
+ * 
+ * Print pattern (E.g., n = 5):
+ * [*---*]
+ * [*---*]
+ * [*-*-*]
+ * [**-**]
+ * [*---*]
+ * 
+ */
+
+public class WShape {
+    public static void main(String[] args) {
+
+        int n = 5;
+        // int n = 7;
+
+        // approach 1
+
+    }
+}
