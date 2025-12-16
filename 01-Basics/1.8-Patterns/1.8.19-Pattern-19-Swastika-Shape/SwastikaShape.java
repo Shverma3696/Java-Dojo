b76d89fcb7bd9d2794cdb3802d45c37b2a6e20af
Shubham Verma <75219986+Shverma3696@users.noreply.github.com>
2025-12-16 11:06:28 +0530

    base pattern

--- 01-Basics/1.8-Patterns/1.8.19-Pattern-19-Swastika-Shape/SwastikaShape.java
+++ 01-Basics/1.8-Patterns/1.8.19-Pattern-19-Swastika-Shape/SwastikaShape.java
@@ -16,6 +16,18 @@ public class SwastikaShape {
         int n = 5;
 
         // approach 1
+        int mid = n / 2 + 1;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i == mid || j == mid) {
+                    System.out.print("*\t");
+                } else {
+                    System.out.print("\t");
+                }
+            }
+            System.out.println();
+        }
 
     }
 }
