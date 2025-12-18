301fdef74fd5991210030bc5a37f2aca65bb831e
Shubham Verma <75219986+Shverma3696@users.noreply.github.com>
2025-12-18 09:28:55 +0530

    calculate nPr

---
+++ 01-Basics/1.9-Functions/1.9.2-Calulate-nPr/NPR.java
@@ -0,0 +1,12 @@
+/**
+ * 1. Compute nPr for giver integers 'n' and 'r' using function
+ * 2. Hint : Permutation formula
+ * nPr = n! / (n-r)!
+ * 
+ */
+
+public class NPR {
+    public static void main(String[] args) {
+
+    }
+}
