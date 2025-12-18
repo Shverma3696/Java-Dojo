761cbce752186d977ea99112d4dc4976560bae69
Shubham Verma <75219986+Shverma3696@users.noreply.github.com>
2025-12-18 09:34:51 +0530

    code running

--- 01-Basics/1.9-Functions/1.9.2-Calulate-nPr/NPR.java
+++ 01-Basics/1.9-Functions/1.9.2-Calulate-nPr/NPR.java
@@ -6,7 +6,24 @@
  */
 
 public class NPR {
+
+    // function to calculate factorial of a number
+    public static int factorial(int x) {
+        int rv = 1; // return value or value that will be return to main after computation
+
+        for (int i = 1; i <= x; i++) {
+            rv *= i;
+        }
+
+        return rv;
+    }
+
     public static void main(String[] args) {
 
+        int n = 5, r = 2;
+
+        int nPr = factorial(n) / factorial(n - r);
+
+        System.out.println("nPr = " + nPr);
     }
 }
