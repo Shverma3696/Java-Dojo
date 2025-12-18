68d66fe496235797ac76b82fb4c9d13f54f9011a
Shubham Verma <75219986+Shverma3696@users.noreply.github.com>
2025-12-18 09:21:20 +0530

    n square using function

---
+++ 01-Basics/1.9-Functions/1.9.1-N-Square/Nsquare.java
@@ -0,0 +1,16 @@
+/**
+ * 1. Compute square of a number using function
+ * 2. given integer 'n'.
+ */
+
+public class Nsquare {
+    public static void main(String[] args) {
+
+        int n = 101;
+
+        // normal way without function
+        int nSquare = n * n;
+
+        System.out.println("N square = " + nSquare);
+    }
+}
