80a6c44d96ba82bd7a7d6dda0995971d803d4ee4
Shubham Verma <75219986+Shverma3696@users.noreply.github.com>
2025-12-19 11:11:18 +0530

    optimised using functions

--- 01-Basics/1.9-Functions/1.9.3-Digit-Frequency/DigitFrequency.java
+++ 01-Basics/1.9-Functions/1.9.3-Digit-Frequency/DigitFrequency.java
@@ -6,23 +6,27 @@
  */
 
 public class DigitFrequency {
+
+    // approach using function
+    public static int computeDigitFrequency(int x, int y) {
+        int frequency = 0;
+
+        while (x != 0) {
+            int rem = x % 10;
+            if (rem == y) {
+                frequency++;
+            }
+            x = x / 10;
+        }
+        return frequency;
+    }
+
     public static void main(String[] args) {
 
         // given
         int n = 994543234, d = 4;
 
-        // approach
-        int frequency = 0;
-
-        while (n != 0) {
-            int rem = n % 10;
-            if (rem == d) {
-                frequency++;
-            }
-            n = n / 10;
-        }
-
-        System.out.println(frequency);
+        System.out.println(computeDigitFrequency(n, d));
 
     }
 
