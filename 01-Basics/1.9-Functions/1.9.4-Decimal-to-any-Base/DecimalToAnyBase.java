be67b7ecce79c9e4e9bf01fd17ea1dd9a3097739
Shubham Verma <75219986+Shverma3696@users.noreply.github.com>
2025-12-20 13:12:27 +0530

    code running

--- 01-Basics/1.9-Functions/1.9.4-Decimal-to-any-Base/DecimalToAnyBase.java
+++ 01-Basics/1.9-Functions/1.9.4-Decimal-to-any-Base/DecimalToAnyBase.java
@@ -8,7 +8,28 @@
 
 public class DecimalToAnyBase {
 
+    public static int decimalToAnyBase(int n, int b) {
+
+        int cnum = 0; // converted number
+        int count = 0;
+        while (n != 0) {
+            int rem = n % b;
+            int mult = (int) Math.pow(10, count);
+            cnum = cnum + rem * mult;
+
+            n = n / b;
+            count++;
+        }
+
+        return cnum;
+    }
+
     public static void main(String[] args) {
+
+        // given
+        int n = 634, b = 8;
+
+        System.out.println(decimalToAnyBase(n, b));
 
     }
 
