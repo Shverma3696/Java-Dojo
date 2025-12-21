965086b2e39a315ca8205e366699b5743471317c
Shubham Verma <75219986+Shverma3696@users.noreply.github.com>
2025-12-21 17:18:12 +0530

    code running

--- 01-Basics/1.9-Functions/1.9.5-Any-Base-to-Decimal/AnyBaseToDecimal.java
+++ 01-Basics/1.9-Functions/1.9.5-Any-Base-to-Decimal/AnyBaseToDecimal.java
@@ -1,6 +1,6 @@
 /**
  * 1. You are grven a number 'n'.
- * 2. You are given a base 'b'. 'n' is a number on base 'b'.
+ * 2. You are given a base 'b' (2 > b < 10). 'n' is a number on base 'b'.
  * 3. You are required to convert the number 'n' into its corresponding value in
  * decimal system.
  * 
@@ -10,46 +10,26 @@ public class AnyBaseToDecimal {
 
     // my approach
     public static int anyBaseToDecimal(int n, int b) {
+        int rv = 0; // return value
+        int pow = 1; // multiplication power of 8
+        while (n > 0) {
+            int dig = n % 10;
+            n = n / 10;
+
+            rv += dig * pow;
+            pow *= 8;
+        }
+
+        return rv;
     }
 
     public static void main(String[] args) {
 
         // given
-        int n = 634, b = 8;
+        int n = 1172, b = 8;
 
         System.out.println(anyBaseToDecimal(n, b));
 
     }
 
 }
-
-/**
- * // my approach
- * public static int decimalToAnyBase(int n, int b) {
- * 
- * int cnum = 0; // converted number
- * int count = 0;
- * while (n != 0) {
- * int rem = n % b;
- * int mult = (int) Math.pow(10, count);
- * cnum = cnum + rem * mult;
- * 
- * n = n / b;
- * count++;
- * }
- * 
- * return cnum;
- * }
- * 
- * // int rv = 0; // return value
- * // int pow = 1; // multiplication power of 10
- * // while (n > 0) {
- * // int dig = n % b;
- * // n = n / b;
- * 
- * // rv += dig * pow;
- * // pow *= 10;
- * // }
- * 
- * // return rv;
- */
