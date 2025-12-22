90f9701c73bb36a9d4d4ddff75fbde2b2687bb1d
Shubham Verma <75219986+Shverma3696@users.noreply.github.com>
2025-12-22 09:35:13 +0530

    any base to any base conversion

---
+++ 01-Basics/1.9-Functions/1.9.6-Any-Base-to-Any-Base/AnyBaseToAnyBase.java
@@ -0,0 +1,36 @@
+/**
+ * 1. You are given a number 'n'.
+ * 2. You are given a base 'b1'. 'n' is a on base 'b1'.
+ * 3. You are given another base 'b2'.
+ * 4. You are required to convert the number 'n' of base 'b1' to a number in
+ * base b2.
+ * 
+ */
+
+public class AnyBaseToAnyBase {
+
+    // my approach
+    public static int anyBaseToAnyBase(int n, int b) {
+        // int rv = 0; // return value
+        // int pow = 1; // multiplication power of 8 to the power 0
+        // while (n > 0) {
+        // int dig = n % 10;
+        // n = n / 10;
+
+        // rv += dig * pow;
+        // pow *= b;
+        // }
+
+        // return rv;
+    }
+
+    public static void main(String[] args) {
+
+        // given
+        int n = 1172, b = 8;
+
+        System.out.println(anyBaseToAnyBase(n, b));
+
+    }
+
+}
