ab8adcc6f59f656b0889395bc70fc94a6fe559c0
Shubham Verma <75219986+Shverma3696@users.noreply.github.com>
2025-12-23 11:58:20 +0530

    any base addition

---
+++ 01-Basics/1.9-Functions/1.9.7-Any-Base-Addition/AnyBaseAddition.java
@@ -0,0 +1,25 @@
+/**
+ * 1. You are given a base 'b'.
+ * 2. You are given two numbers 'n1' and 'n2' of base b.
+ * 3. You are required to add the two numbes and print their value in base b.
+ * 
+ */
+
+public class AnyBaseAddition {
+
+    public static void main(String[] args) {
+
+        // given
+        int n1 = 236, n2 = 754, b = 8;
+
+        System.out.println(getSum(n1, n2, b));
+
+    }
+
+    // my approach
+    public static int getSum(int n1, int n2, int b) {
+        int rv = 0; // return value
+
+        return rv;
+    }
+}
