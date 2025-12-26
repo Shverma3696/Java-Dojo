9591ebe722e56d10186566d3b7c962597f67ebab
Shubham Verma <75219986+Shverma3696@users.noreply.github.com>
2025-12-26 20:51:52 +0530

    get length of an array

--- 02-Arrays/2.1-Array-Basics/ArrayBasics.java
+++ 02-Arrays/2.1-Array-Basics/ArrayBasics.java
@@ -6,5 +6,22 @@
 public class ArrayBasics {
     public static void main(String[] args) {
 
+        // 1. 2 lines approach
+        // declaring
+        // int[] arr;
+
+        // initializing
+        // arr = new int[5];
+
+        // 2. 1 line approach
+        int[] arr = new int[5];
+        arr[0] = 51;
+        arr[1] = 45;
+        arr[2] = 80;
+        arr[3] = 75;
+        arr[4] = 65;
+
+        // get array of length using arr.length
+        System.out.println(arr.length);
     }
 }
