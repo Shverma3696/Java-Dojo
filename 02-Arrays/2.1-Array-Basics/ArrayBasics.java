24ff816f8ed8b22a9d196bdff6d56482a56aaabe
Shubham Verma <75219986+Shverma3696@users.noreply.github.com>
2025-12-27 13:39:57 +0530

    understood memory structure of arrays

--- 02-Arrays/2.1-Array-Basics/ArrayBasics.java
+++ 02-Arrays/2.1-Array-Basics/ArrayBasics.java
@@ -46,5 +46,37 @@ public class ArrayBasics {
         for (int i = 0; i < arr2.length; i++) {
             System.out.print(arr2[i] + " ");
         }
+        System.out.println();
+
+        // checking what happens with function calls
+        swap(arr2, 0, 4);
+
+        // check and print arr 1 first then arr2 again
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+        for (int i = 0; i < arr2.length; i++) {
+            System.out.print(arr2[i] + " ");
+        }
+
+    }
+
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 }
+
+/**
+ * when an array is declared a variable of its name is created in STACK
+ * with NULL as its default value
+ * 
+ * However, when the same array is initialized:
+ * - in the STACK; the value null changes to the HEAP address
+ * - in the HEAP; the full array elements is stored in the heap
+ * 
+ * Therefore, the array variable in the stack is simply pointing to the memory
+ * address in the heap and all the elements are in the HEAP itself.
+ */
