252370f733c96115c70306e20ed7ef1b484b61a5
Shubham Verma <75219986+Shverma3696@users.noreply.github.com>
2025-12-28 10:31:21 +0530

    findind element in an array

---
+++ 02-Arrays/2.3-Find-Element-in-an-Array/FindElementInAnArray.java
@@ -0,0 +1,15 @@
+/**
+ * 1. You are given a number 'n', representing the size of array 'a'.
+ * 2. You are given 'n' numbers representing the elements of array 'a'.
+ * 3. You are given another number 'd'.
+ * 4. You are required to check if 'd' exists in the array 'a' and at what index
+ * (0 based). If found print the index; otherwise print -1.
+ * 
+ */
+
+public class FindElementInAnArray {
+    public static void main(String[] args) {
+        // given
+        int[] arr = { 15, 30, 40, 4, 11, 9 };
+    }
+}
