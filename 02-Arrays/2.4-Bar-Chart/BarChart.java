4da6cb806ab304f4dbc1584fa57d7e1cbd293fd8
Shubham Verma <75219986+Shverma3696@users.noreply.github.com>
2025-12-29 10:28:26 +0530

    bar chart

---
+++ 02-Arrays/2.4-Bar-Chart/BarChart.java
@@ -0,0 +1,22 @@
+/**
+ * 1. You are given a number 'n', representing the size of array 'a'.
+ * 2. You are given 'n' numbers representing the elements of array 'a'.
+ * 3. You are required to print a vertical bar chart where '*' will represent
+ * the single unit and total height of bar is the value of element of array 'a'
+ * at that index.
+ * 4. Bar should be in the same order at the elements
+ * 
+ */
+
+public class BarChart {
+    public static void main(String[] args) {
+
+        // given
+        int[] a = { 15, 30, 40, 4, 11, 9 };
+        int d = 40; // element to find
+
+        // System.out.println(d + " Element is at the following index => " +
+        // findIndexOfElementInAnArray(a, d));
+    }
+
+}
