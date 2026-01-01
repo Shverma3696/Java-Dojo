ef23565d7a9b8ddfa9c95c049aad87391d95d186
Shubham Verma <75219986+Shverma3696@users.noreply.github.com>
2026-01-01 12:42:24 +0000

    reversing the elements of an array

---
+++ 02-Arrays/2.7-Reverse-an-Array/ReverseAnArray.java
@@ -0,0 +1,18 @@
+/**
+ * 1. You are given a number 'n', representing the size of array 'a'.
+ * 2. You are given 'n' numbers, representing elements of array 'a'.
+ * 3. You are required to reverse the content of array 'a'.
+ * 
+ */
+
+public class ReverseAnArray {
+    public static void main(String[] args) {
+
+        // given
+        int[] a = { 3, 1, 0, 7, 5 };
+
+        // funtions
+        reverse(a);
+        display(a);
+    }
+}
