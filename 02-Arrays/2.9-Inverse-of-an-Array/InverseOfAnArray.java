10104a1c393c9f4a4bac92c2d6e1b554034c2c11
Shubham Verma <75219986+Shverma3696@users.noreply.github.com>
2026-01-03 09:06:42 +0000

    removed extra comments

--- 02-Arrays/2.9-Inverse-of-an-Array/InverseOfAnArray.java
+++ 02-Arrays/2.9-Inverse-of-an-Array/InverseOfAnArray.java
@@ -29,45 +29,6 @@ public class InverseOfAnArray {
         return rv;
     }
 
-    // public static void rotate(int[] a, int k) {
-    // // to roate an array; the approach is as follows
-    // // divide the array in 2 parts
-    // // part1 = {0 - a.length-k-1} reverse these elements
-    // // part2 = {a.length-k to a.length-1} reverse these elements
-    // // once above is done, part3 is to reverse the full array as a result we will
-    // // get the expected output
-
-    // // handle k
-    // k = k % a.length;
-    // if (k < 0) {
-    // k = k + a.length;
-    // }
-
-    // // part 1
-    // reverse(a, 0, a.length - k - 1);
-
-    // // part 2
-    // reverse(a, a.length - k, a.length - 1);
-
-    // // part 3
-    // reverse(a, 0, a.length - 1);
-    // }
-
-    // public static void reverse(int[] a, int i, int j) {
-    // int li = i; // left index
-    // int ri = j; // right index
-
-    // while (li < ri) {
-    // int temp = a[li];
-    // a[li] = a[ri];
-    // a[ri] = temp;
-
-    // li++;
-    // ri--;
-    // }
-
-    // }
-
     public static void display(int[] a) {
         for (int val : a) {
             System.out.print(val + " ");
