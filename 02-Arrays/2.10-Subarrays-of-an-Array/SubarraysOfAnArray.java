43dfec58480eab33aeee09745e6bd94575062ee3
Shubham Verma <75219986+Shverma3696@users.noreply.github.com>
2026-01-04 09:59:14 +0000

    folder restructure and added a new question subarrays of an array

---
+++ 02-Arrays/2.10-Subarrays-of-an-Array/SubarraysOfAnArray.java
@@ -0,0 +1,22 @@
+/**
+ * 1. You are given a number 'n', representing the size of array 'a'.
+ * 2. You are given 'n' numbers, representing elements of array 'a'.
+ * 3. You are required to print all subarrays of the given array 'a'.
+ * 4. Constraints : 1 >= n <= 10
+ * 
+ */
+
+public class SubarraysOfAnArray {
+    public static void main(String[] args) {
+
+        // given
+        int[] a = { 10, 20, 30 };
+
+    }
+
+    // public static void display(int[] a) {
+    // for (int val : a) {
+    // System.out.print(val + " ");
+    // }
+    // }
+}
