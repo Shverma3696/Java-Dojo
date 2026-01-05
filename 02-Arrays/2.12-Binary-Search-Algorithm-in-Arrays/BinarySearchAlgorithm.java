2cb1a15c81b6470347f4e1521380720728d81785
Shubham Verma <75219986+Shverma3696@users.noreply.github.com>
2026-01-05 09:01:27 +0000

    Binary Search Algo implementation

---
+++ 02-Arrays/2.12-Binary-Search-Algorithm-in-Arrays/BinarySearchAlgorithm.java
@@ -0,0 +1,23 @@
+/**
+ * 1. You are given a number 'n', representing the size of array 'a'.
+ * 2. You are given 'n' numbers, representing elements of array 'a'.
+ * 3. Elements of array are sorted
+ * 4. You are required to search an element/data/value in the arrays if it is
+ * present or not.
+ * 
+ * Hint: We are using "Binary Search Algorithm" here
+ * 
+ * Note:- we can also do this with Linear Search but it takes more time because
+ * we will have iterate over every single element and compare and find the data.
+ * So, since the array provided is sorted we can easliy use Binary Search
+ * 
+ */
+
+public class BinarySearchAlgorithm {
+    public static void main(String[] args) {
+
+        // given
+        int[] a = { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100 };
+    }
+
+}
