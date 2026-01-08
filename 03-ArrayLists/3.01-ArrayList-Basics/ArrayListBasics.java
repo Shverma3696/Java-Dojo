9748cdc0b6e6b8507a10e9bf7423e67729480876
Shubham Verma <75219986+Shverma3696@users.noreply.github.com>
2026-01-08 22:10:24 +0000

    declaring and printing AL

--- 03-ArrayLists/3.01-ArrayList-Basics/ArrayListBasics.java
+++ 03-ArrayLists/3.01-ArrayList-Basics/ArrayListBasics.java
@@ -1,10 +1,24 @@
+
 /**
  * 1. Practicing basics of ArrayLists
+ * - Declaring
+ * - Initialising
+ * - Printing
+ * - get size()
  * 
- */
+ * Difference between Arrays and ArrayLists
+ * -> Array = Immutable
+ * -> ArrayList = Mutable / can change or modify
+ * 
+*/
+
+import java.util.ArrayList;
 
 public class ArrayListBasics {
     public static void main(String[] args) {
 
+        // 1. Declaring and Initialising an ArrayList
+        ArrayList<Integer> list = new ArrayList<>();
+        System.out.println(list + " -> " + list.size()); // output : [] (meaning empty) -> 0 (size 0)
     }
 }
