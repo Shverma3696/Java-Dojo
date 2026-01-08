e0f8807286824b358827e411699194686acd6fa4
Shubham Verma <75219986+Shverma3696@users.noreply.github.com>
2026-01-08 22:16:51 +0000

    adding element in AL at some index

--- 03-ArrayLists/3.01-ArrayList-Basics/ArrayListBasics.java
+++ 03-ArrayLists/3.01-ArrayList-Basics/ArrayListBasics.java
@@ -20,5 +20,16 @@ public class ArrayListBasics {
         // 1. Declaring and Initialising an ArrayList
         ArrayList<Integer> list = new ArrayList<>();
         System.out.println(list + " -> " + list.size()); // output : [] (meaning empty) -> 0 (size 0)
+
+        // 2. Adding elements to the ArrayList
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        System.out.println(list + " -> " + list.size()); // output : [10, 20, 30] -> 3
+
+        // 3. add or insert an element in between at some index
+        list.add(1, 1000); // or
+        System.out.println(list + " -> " + list.size()); // output : [10, 1000, 20, 30] -> 4
+
     }
 }
