e7933349fee37ce2e30deb25fcd2cb5f7ec9e0b6
Shubham Verma <75219986+Shverma3696@users.noreply.github.com>
2026-01-09 23:01:48 +0000

    removing primes numbers from arraylist

---
+++ 03-ArrayLists/3.02-Removing-Prime-Numbers-from-AL/RemovingPrimesFromArrayList.java
@@ -0,0 +1,32 @@
+
+/**
+ * 1. You are given an ArrayList of positive integers.
+ * 2. You have to remove prime numbers from the given ArrayList and return the updated ArrayList.
+ * 
+ * Note:- The order of the elements should remain same.
+ * 
+*/
+
+import java.util.ArrayList;
+
+public class RemovingPrimesFromArrayList {
+    public static void main(String[] args) {
+
+        // new way of Declaring and Initialising an ArrayList
+        ArrayList<Integer> list = new ArrayList<>() {
+            {
+                add(1);
+                add(2);
+                add(3);
+                add(4);
+                add(5);
+                add(6);
+                add(7);
+                add(8);
+                add(9);
+                add(10);
+            }
+        };
+
+    }
+}
