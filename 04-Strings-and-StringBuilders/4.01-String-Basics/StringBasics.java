102936258b554989f59b1bfef04bb043ae9ebdc3
Shubham Verma <75219986+Shverma3696@users.noreply.github.com>
2026-01-10 23:49:18 +0000

    charAt() and substrings

--- 04-Strings-and-StringBuilders/4.01-String-Basics/StringBasics.java
+++ 04-Strings-and-StringBuilders/4.01-String-Basics/StringBasics.java
@@ -1,13 +1,15 @@
-import java.util.Scanner;
-
 /**
  * String Basics
  * - Declaring
  * - Initialising
  * - taking input from console
  * - length()
- * 
+ * - charAt()
+ * - substring
  */
+
+// import java.util.Scanner;
+
 public class StringBasics {
     public static void main(String[] args) {
         // Scanner scanner = new Scanner(System.in);
@@ -27,5 +29,21 @@ public class StringBasics {
         // 3. Length90 function - gets the size of string
         String s = "abcdef";
         System.out.println("String size = " + s.length());
+
+        // 4. charAt() - gets character of string at a given index
+        char ch = s.charAt(3);
+        System.out.println(ch); // 4.1. printing 1 character
+
+        // 4.2. printing all characters
+        for (int i = 0; i < s.length(); i++) {
+            System.out.println(s.charAt(i));
+        }
+
+        // 5. SubString
+        // 5.1. only start index
+        System.out.println(s.substring(1)); // output = bcdef; all chars from 1st index (included)
+        // 5.2. start and end index both
+        System.out.println(s.substring(1, 5)); // output = bcde - endindex is exclusive or not included
+        System.out.println(s.substring(1, 1)); // output = "" - blank because of endindex
     }
 }
