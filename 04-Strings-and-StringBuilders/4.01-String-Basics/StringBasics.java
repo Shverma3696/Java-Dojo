8d7d606c926abc88a5ce4902f718fcd428e7a4a3
Shubham Verma <75219986+Shverma3696@users.noreply.github.com>
2026-01-10 23:35:19 +0000

    string length()

--- 04-Strings-and-StringBuilders/4.01-String-Basics/StringBasics.java
+++ 04-Strings-and-StringBuilders/4.01-String-Basics/StringBasics.java
@@ -5,14 +5,27 @@ import java.util.Scanner;
  * - Declaring
  * - Initialising
  * - taking input from console
+ * - length()
  * 
  */
 public class StringBasics {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        // Scanner scanner = new Scanner(System.in);
 
         // 1. Declaring and initialising Strings
-        String s = "Hello";
-        System.out.println(s);
+        // String s1 = "Hello";
+        // System.out.println(s1);
+
+        // 2. taking input from console
+        // System.out.println("enter string to input :");
+        // String s2 = scanner.next(); // next() reads elements till first space, if
+        // more than 2 elements then create a new String variable and again use next()
+        // space
+        // String s2 = scanner.nextLine(); // nextLine() reads entire line
+        // System.out.println(s2);
+
+        // 3. Length90 function - gets the size of string
+        String s = "abcdef";
+        System.out.println("String size = " + s.length());
     }
 }
