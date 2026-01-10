88d23d7c1df2e21a70a753145af52b84dba5ff69
Shubham Verma <75219986+Shverma3696@users.noreply.github.com>
2026-01-10 23:18:56 +0000

    string basics

---
+++ 04-Strings-and-StringBuilders/4.01-String-Basics/StringBasics.java
@@ -0,0 +1,12 @@
+/**
+ * String Basics
+ * - Declaring
+ * - Initialising
+ * - taking input from console
+ * 
+ */
+public class StringBasics {
+    public static void main(String[] args) {
+
+    }
+}
