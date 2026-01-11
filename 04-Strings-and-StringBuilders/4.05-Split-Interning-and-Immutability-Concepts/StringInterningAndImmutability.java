22c876fa44b9f7c4c1a2c19fa6c826390631ac17
Shubham Verma <75219986+Shverma3696@users.noreply.github.com>
2026-01-11 18:06:45 +0000

    string comparison 2

--- 04-Strings-and-StringBuilders/4.05-Split-Interning-and-Immutability-Concepts/StringInterningAndImmutability.java
+++ 04-Strings-and-StringBuilders/4.05-Split-Interning-and-Immutability-Concepts/StringInterningAndImmutability.java
@@ -4,6 +4,11 @@
  * - String Interning
  * - String with 'new' keyword
  * 
+ * 
+ * Please Note:- Always use equals() method for string comparisons as it check
+ * both the first the address, if same return true, else checks the string char
+ * by char and only then return true or false
+ * 
  */
 
 public class StringInterningAndImmutability {
@@ -18,6 +23,14 @@ public class StringInterningAndImmutability {
         // -> Does not check and compare the string character by character
         System.out.println(s1 == s2); // true, since both pointing to same address in memory or intern pool
         System.out.println(s1 == s3); // false, since s3 is created using 'new' keyword it point to different address
+
+        // 2. Comparison using 'equals()';
+        // -> checks and compares, first the address to which the variable is pointing,
+        // if same returns true else,
+        // -> checks and compares the string character by character
+        System.out.println(s1.equals(s2)); // true, since both pointing to same address in memory or intern pool
+        System.out.println(s1.equals(s3)); // true, address maybe different but equals() method also check the string
+                                           // char by char
 
     }
 }
