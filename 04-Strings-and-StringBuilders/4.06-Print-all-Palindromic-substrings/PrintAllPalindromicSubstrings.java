dadc1d1e9002829dfa057e0bb0fce176d53e9e97
Shubham Verma <75219986+Shverma3696@users.noreply.github.com>
2026-01-12 09:06:43 +0000

    print all pallindromic substrings

---
+++ 04-Strings-and-StringBuilders/4.06-Print-all-Palindromic-substrings/PrintAllPalindromicSubstrings.java
@@ -0,0 +1,31 @@
+/**
+ * 1. Given a String 's'.
+ * 2. You are required to print all palindromic substrings of the given string
+ * 
+ */
+
+public class PrintAllPalindromicSubstrings {
+    public static void main(String[] args) {
+
+        String s = "abccbc";
+
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = i + 1; j <= s.length(); j++) {
+                String ss = s.substring(i, j);
+                if (isSubstringPalindromic(ss)) {
+                    System.out.println(ss);
+                }
+            }
+        }
+    }
+
+    public static boolean isSubstringPalindromic(String str) {
+        int i = 0;
+        int j = str.length() - 1;
+        while (str.charAt(i) == str.charAt(j)) {
+
+            i++;
+            j--;
+        }
+    }
+}
