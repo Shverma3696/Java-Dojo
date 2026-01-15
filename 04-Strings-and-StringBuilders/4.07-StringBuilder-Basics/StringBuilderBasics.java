4537fb8e2bb06031ac60efd123a5e8f8ad7c4a12
Shubham Verma <75219986+Shverma3696@users.noreply.github.com>
2026-01-16 01:33:56 +0530

    performance comparison of strings and stringbuilder class

--- 04-Strings-and-StringBuilders/4.07-StringBuilder-Basics/StringBuilderBasics.java
+++ 04-Strings-and-StringBuilders/4.07-StringBuilder-Basics/StringBuilderBasics.java
@@ -6,11 +6,13 @@
  * - length()
  * - charAt()
  * - setCharAt()
+ * - insert()
  * - substring
  * 
  * Importnant Note:-
- * StringBuilder class is mutable and provide built-in methods like insert() and
- * setCharAt()
+ * >> StringBuilder class is mutable and provide built-in methods like insert()
+ * and setCharAt()
+ * >> Performance is better compared to strings (see problem 4.08)
  * 
  */
 
@@ -33,7 +35,27 @@ public class StringBuilderBasics {
 
         // 5. setCharAt() method : changes the instance/character at given index
         sb.setCharAt(0, 'C');
-        System.out.println("Modified String = " + sb);
+        System.out.println("1. Modified String (setCharAt) = " + sb);
+
+        // 6. Insert() method - inserts the char at the given index and shift/changes
+        // the index of the following chars of string
+        sb.insert(2, 'y');
+        System.out.println("2. Modified String (insert) = " + sb);
+
+        // 7. remove or delete chars
+        // 7.1. method 1 -> deleteCharAt(<index>); deletes char at given index
+        sb.deleteCharAt(2);
+        System.out.println("3. Modified String (deleteCharAt) = " + sb);
+
+        // 7.2. method 2 -> delete(<start-index>, <end-index>); end-index is exclusive;
+        // deletes the substring between the given indexes
+        // treat end index as [end-1]
+        sb.delete(5, 12);
+        System.out.println("4. Modified String (delete-substring-startIdx-endIdx) = " + sb);
+
+        // 8. Substring in stringbuilder
+        String ss = sb.substring(3, 5);
+        System.out.println("substring = " + ss);
 
     }
 }
---
+++ 04-Strings-and-StringBuilders/4.08-Performance-Comparison-Str-n-SB/String_vs_StringBuilder.java
@@ -0,0 +1,45 @@
+/**
+ * Comparing performance for large operations in strings and StringBuilder
+ * performance metric = execution time
+ * 
+ * Note:-
+ * -> String take O(n^2) time because String are immutable so everytime a char
+ * is added to the same string a copy of that string is generated/created first
+ * and then the new char is added to that
+ * 
+ * vs
+ * 
+ * -> StringBuilder takes only O(n) time because it is a mutable class, it
+ * simply adds the new char in the same string
+ * 
+ */
+
+public class String_vs_StringBuilder {
+    public static void main(String[] args) {
+
+        // given a large integer
+        int n = 100000; // result = approx 3500ms (string) vs less than 50ms (StringBuilder)
+
+        // 1. String
+        String str = "";
+        long startTime1 = System.currentTimeMillis();
+        for (int i = 0; i < n; i++) {
+            str += i;
+        }
+        long endTime1 = System.currentTimeMillis();
+
+        long durationString = endTime1 - startTime1;
+        System.out.println("Execution time for String = " + durationString);
+
+        // 2. StringBuilder
+        StringBuilder sb = new StringBuilder();
+        long startTime2 = System.currentTimeMillis();
+        for (int i = 0; i < n; i++) {
+            sb.append(i);
+        }
+        long endTime2 = System.currentTimeMillis();
+
+        long durationStringBuilder = endTime2 - startTime2;
+        System.out.println("Execution time for StringBuilder = " + durationStringBuilder);
+    }
+}
