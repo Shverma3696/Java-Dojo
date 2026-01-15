d9b5f4a6714e267daf0e4b359e0c083bf161023d
Shubham Verma <75219986+Shverma3696@users.noreply.github.com>
2026-01-16 00:32:51 +0530

    String builder class

--- 04-Strings-and-StringBuilders/4.07-StringBuilder-Basics/StringBuilderBasics.java
+++ 04-Strings-and-StringBuilders/4.07-StringBuilder-Basics/StringBuilderBasics.java
@@ -13,5 +13,13 @@
 
 public class StringBuilderBasics {
     public static void main(String[] args) {
+
+        // 1. Declaring and initialising
+        StringBuilder sb = new StringBuilder("Hello-World");
+        System.out.println(sb);
+
+        // 2. charAt() method
+        char ch = sb.charAt(0);
+        System.out.println("Char at given index = " + ch);
     }
 }
