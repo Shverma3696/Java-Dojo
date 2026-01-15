31b800c53948c5ee10023a4ed410fa1dce2a8732
Shubham Verma <75219986+Shverma3696@users.noreply.github.com>
2026-01-16 00:40:18 +0530

    more about stringbuilder

--- 04-Strings-and-StringBuilders/4.07-StringBuilder-Basics/StringBuilderBasics.java
+++ 04-Strings-and-StringBuilders/4.07-StringBuilder-Basics/StringBuilderBasics.java
@@ -18,7 +18,13 @@ public class StringBuilderBasics {
         StringBuilder sb = new StringBuilder("Hello-World");
         System.out.println(sb);
 
-        // 2. charAt() method
+        // 2. Taking input from console is similar to Strings
+
+        // 3. Length() of sb
+        int lengthOfStringBuilder = sb.length();
+        System.out.println("Length of SB = " + lengthOfStringBuilder);
+
+        // 4. charAt() method
         char ch = sb.charAt(0);
         System.out.println("Char at given index = " + ch);
     }
