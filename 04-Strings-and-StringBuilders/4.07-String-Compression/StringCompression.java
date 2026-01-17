0e028622d61b483d88540a029b1c6c7dc79d370d
Shubham Verma <75219986+Shverma3696@users.noreply.github.com>
2026-01-17 10:17:55 +0530

    string compression code structure

--- 04-Strings-and-StringBuilders/4.07-String-Compression/StringCompression.java
+++ 04-Strings-and-StringBuilders/4.07-String-Compression/StringCompression.java
@@ -1,13 +1,22 @@
 /**
- * 1. Given a String 's'.
- * 2. You are required to print all palindromic substrings of the given string
+ * 1. Given a String 'str'.
+ * 2. You are required to print compression1 and compression2 of given string
+ * 
+ * E.g., str = "aaabbcccddaeef"
+ * >> compression1 = abcdaef
+ * >> compression2 = a3b2c3d2ae2f (number of times the same char occurs except 1
+ * time)
  * 
  */
 
 public class StringCompression {
     public static void main(String[] args) {
 
-        String s = "abccbc";
+        String s = "aaabbcccddaeef";
+
+        compression1(str);
+        compression2(str);
 
     }
+
 }
