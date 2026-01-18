1bd3e6b47ed3ab588466dacc77f45c77fdc6994c
Shubham Verma <75219986+Shverma3696@users.noreply.github.com>
2026-01-18 16:14:56 +0530

    comments updated

--- 04-Strings-and-StringBuilders/4.10-Toggle-Case-String/ToggleCase.java
+++ 04-Strings-and-StringBuilders/4.10-Toggle-Case-String/ToggleCase.java
@@ -19,12 +19,15 @@ public class ToggleCase {
         for (int i = 0; i < sb.length(); i++) {
             char ch = sb.charAt(i);
 
-            if (ch >= 'a' && ch <= 'z') {
-                char upperCh = (char) ('A' + ch - 'a'); // upper case character
-                sb.setCharAt(i, upperCh);
-            } else if (ch >= 'A' && ch <= 'Z') {
-                char lowerCh = (char) ('a' + ch - 'A');
-                sb.setCharAt(i, lowerCh);
+            if (ch >= 'a' && ch <= 'z') { // if char is lower case
+
+                char upperCh = (char) ('A' + ch - 'a'); // converted to upper case character
+                sb.setCharAt(i, upperCh); // added or set back the converted char at same position
+
+            } else if (ch >= 'A' && ch <= 'Z') { // if char is uppercase
+
+                char lowerCh = (char) ('a' + ch - 'A'); // converted to lower case
+                sb.setCharAt(i, lowerCh); // added or set back the converted char at same position
             }
         }
 
