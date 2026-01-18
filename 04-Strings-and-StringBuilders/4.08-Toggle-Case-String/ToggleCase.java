2cdcb41e166e0d63d880178b509b911188f1b7ab
Shubham Verma <75219986+Shverma3696@users.noreply.github.com>
2026-01-18 15:58:24 +0530

    folder restructure and toggle case in a string

---
+++ 04-Strings-and-StringBuilders/4.08-Toggle-Case-String/ToggleCase.java
@@ -0,0 +1,16 @@
+/**
+ * 1. Given a string containg lower case and uper case alphabets
+ * 2. You have to toggle the case of every character of the given String.
+ * i.e., lower -> upper ; upper -> lower
+ * E.g., ABcdE -> abCDe
+ * 
+ */
+
+public class ToggleCase {
+    public static void main(String[] args) {
+
+        String str = "ABcdEFgh";
+
+    }
+
+}
