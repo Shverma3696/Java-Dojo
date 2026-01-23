918bcb3b511d3f3c947fd6a5baa6eb7644ad7bad
Shubham Verma <75219986+Shverma3696@users.noreply.github.com>
2026-01-23 22:51:49 +0000

    code structure updated

--- 04-Strings-and-StringBuilders/4.12-Permutations-of-a-String/StringPermutations.java
+++ 04-Strings-and-StringBuilders/4.12-Permutations-of-a-String/StringPermutations.java
@@ -2,12 +2,22 @@
  * 1. Given a string
  * 2. You have to print all permutationsof the given string iteratively
  * 
+ * E.g., given string = abc
+ * output = abc, acb, bac, bca, cab, cba
+ * 
+ * total number of permutations = 3! = 6
+ * hint : length of string factorial
+ * 
  */
 
 public class StringPermutations {
     public static void main(String[] args) {
 
         String str = "abc";
-        // System.out.println(asciiDifferenceString(str));
+        permutationsOfString(str);
+    }
+
+    public static void permutationsOfString(String str) {
+
     }
 }
