b7be247509b8393134b98454b0bb97788c330429
Shubham Verma <75219986+Shverma3696@users.noreply.github.com>
2026-01-23 22:41:42 +0000

    new problem permutation of a string

---
+++ 04-Strings-and-StringBuilders/4.12-Permutations-of-a-String/StringPermutations.java
@@ -0,0 +1,12 @@
+/**
+ * 1. Given a string
+ * 
+ */
+
+public class StringPermutations {
+    public static void main(String[] args) {
+
+        String str = "abc";
+        // System.out.println(asciiDifferenceString(str));
+    }
+}
