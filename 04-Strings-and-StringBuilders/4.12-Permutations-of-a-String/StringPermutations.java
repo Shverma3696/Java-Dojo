6b536ccc0e126f7c5083f0273aaf80bb9d98fae7
Shubham Verma <75219986+Shverma3696@users.noreply.github.com>
2026-01-23 23:02:33 +0000

    code running

--- 04-Strings-and-StringBuilders/4.12-Permutations-of-a-String/StringPermutations.java
+++ 04-Strings-and-StringBuilders/4.12-Permutations-of-a-String/StringPermutations.java
@@ -18,6 +18,31 @@ public class StringPermutations {
     }
 
     public static void permutationsOfString(String str) {
+        int n = str.length();
+        int fact = factorial(n);
 
+        for (int i = 0; i < fact; i++) {
+            int temp = i;
+            StringBuilder sb = new StringBuilder(str);
+
+            for (int div = n; div >= 1; div--) { // divisor
+                int quo = temp / div; // quotiant
+                int rem = temp % div; // remainder
+
+                System.out.print(sb.charAt(rem));
+                sb.deleteCharAt(rem);
+
+                temp = quo;
+            }
+            System.out.println();
+        }
+    }
+
+    public static int factorial(int n) {
+        int val = 1;
+        for (int i = 2; i <= n; i++) {
+            val *= i;
+        }
+        return val;
     }
 }
