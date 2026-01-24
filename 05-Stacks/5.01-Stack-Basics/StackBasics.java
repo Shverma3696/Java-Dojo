35cc7b1130d028f8374e7d88b961c2c24d54c526
Shubham Verma <75219986+Shverma3696@users.noreply.github.com>
2026-01-24 21:02:41 +0000

    push method in stack

--- 05-Stacks/5.01-Stack-Basics/StackBasics.java
+++ 05-Stacks/5.01-Stack-Basics/StackBasics.java
@@ -1,15 +1,30 @@
+import java.util.Stack;
+
 /**
  * Stack Basics
  * - Declaring
  * - Initialising
  * - taking input from console
- *
+ * 
+ * 
+ * Similar methods but different name:
+ * add -> push()
+ * get -> peek() ; lets the user check the top / latest element
+ * remove -> pop()
+ * size()
  */
 
 public class StackBasics {
     public static void main(String[] args) {
 
         // 1. Declaring and initialising Stacks
+        Stack<Integer> stk = new Stack<>();
 
+        // 2. Basic methods and usage
+
+        // 2.1. push() method
+        stk.push(10);
+        stk.push(20);
+        System.out.println(stk);
     }
 }
