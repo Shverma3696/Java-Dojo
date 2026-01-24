9ade056f34afb5342eeef7f24a5d7b5048be668c
Shubham Verma <75219986+Shverma3696@users.noreply.github.com>
2026-01-24 20:50:05 +0000

    stacks basics and its usage

---
+++ 05-Stacks/5.01-Stack-Basics/StackBasics.java
@@ -0,0 +1,15 @@
+/**
+ * Stack Basics
+ * - Declaring
+ * - Initialising
+ * - taking input from console
+ *
+ */
+
+public class StackBasics {
+    public static void main(String[] args) {
+
+        // 1. Declaring and initialising Stacks
+
+    }
+}
