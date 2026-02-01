da40d9802dc4184f5392dcd5a38b50ca36239799
Shubham Verma <75219986+Shverma3696@users.noreply.github.com>
2026-02-01 11:01:38 +0000

    largest area histogram

---
+++ 05-Stacks/5.09-Largest-Area-Histogram-Stack/LargestAreaHistogram.java
@@ -0,0 +1,30 @@
+
+/**
+ * 1. You are given a number 'n', representing the size of array 'a'.
+ * 2. You are given 'n' numbers, representing the heights of bars in a bar chart
+ * prices of a share on 'n' days.
+ * 3. You are required to find and print the area of largest rectangle in the
+ * histogram.
+ * 
+ * E.g., for array [6 2 5 4 5 1 6] 
+ * => largest area =12
+ * 
+*/
+
+import java.util.Stack;
+
+public class LargestAreaHistogram {
+    public static void main(String[] args) {
+        int[] arr = { 6, 2, 5, 4, 5, 1, 6 };
+
+        int largestArea = solution(arr);
+
+    }
+
+    public static int solution(int[] arr) {
+        int val = 0;
+
+        return val;
+    }
+
+}
