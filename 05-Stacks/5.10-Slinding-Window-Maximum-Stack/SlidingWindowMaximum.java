7343b8cb51d703cf3d4a5b03afd9b6da64d6f482
Shubham Verma <75219986+Shverma3696@users.noreply.github.com>
2026-02-10 23:04:04 +0000

    code

--- 05-Stacks/5.10-Slinding-Window-Maximum-Stack/SlidingWindowMaximum.java
+++ 05-Stacks/5.10-Slinding-Window-Maximum-Stack/SlidingWindowMaximum.java
@@ -16,7 +16,42 @@ import java.util.Stack;
 public class SlidingWindowMaximum {
     public static void main(String[] args) {
         int[] arr = { 2, 9, 3, 8, 1, 7, 12, 6, 14, 4, 32, 0, 7, 19, 8, 12, 6 };
+        int k = 4;
 
+        Stack<Integer> st = new Stack<>();
+        int[] nge = new int[arr.length];
+
+        st.push(arr.length - 1);
+        nge[arr.length - 1] = arr.length;
+
+        for (int i = arr.length - 2; i >= 0; i--) {
+            // pops - answer - push
+
+            while (st.size() > 0 && arr[i] >= arr[st.peek()]) {
+                st.pop();
+            }
+
+            if (st.size() == 0) {
+                nge[i] = arr.length;
+            } else {
+                nge[i] = st.peek();
+            }
+
+            st.push(i);
+        }
+
+        int j = 0;
+        for (int i = 0; i <= arr.length - k; i++) {
+            // enter the loop to find the maximum of window starting at i
+
+            if (j < i) {
+                j = i;
+            }
+            while (nge[j] < i + k) {
+                j = nge[j];
+            }
+            System.out.println(arr[j]);
+        }
     }
 
 }
