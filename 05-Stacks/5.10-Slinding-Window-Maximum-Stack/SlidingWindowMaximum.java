32a22bc86ae377d5ae4e736542011a9dabc63db1
Shubham Verma <75219986+Shverma3696@users.noreply.github.com>
2026-02-10 22:40:20 +0000

    sliding window maximum - new stack problem

---
+++ 05-Stacks/5.10-Slinding-Window-Maximum-Stack/SlidingWindowMaximum.java
@@ -0,0 +1,22 @@
+
+/**
+ * 1. You are given a number 'n'. representing see of array 'a'.
+ * 2. You are given 'n' numbers, representing the elements of array 'a'.
+ * 3. You are given a number 'k'. representing the size of window.
+ * 4. You are required to find and print the maximum element in every window of size 'k'
+ * 
+ * E.g.,
+ * for the array [2 9 3 8 1 7 12 6 14 4 32 0 7 19 8 12 6] and k = 4, 
+ * >> the answer [9 9 8 12 12 14 14 32 32 32 32 19 19 19]
+ * 
+*/
+
+import java.util.Stack;
+
+public class SlidingWindowMaximum {
+    public static void main(String[] args) {
+        int[] arr = { 2, 9, 3, 8, 1, 7, 12, 6, 14, 4, 32, 0, 7, 19, 8, 12, 6 };
+
+    }
+
+}
