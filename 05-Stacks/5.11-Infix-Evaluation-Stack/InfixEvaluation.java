488070c0398750ea776c2ad6173ef3813692fcc1
Shubham Verma <75219986+Shverma3696@users.noreply.github.com>
2026-02-11 22:50:51 +0000

    infix evaluation

---
+++ 05-Stacks/5.11-Infix-Evaluation-Stack/InfixEvaluation.java
@@ -0,0 +1,17 @@
+
+/**
+ * 1. You are given an infix expression.
+ * 2. You are required to evaluate and print its value
+ * 
+ * E.g.,
+ * expression = 2 + 6 * 4 / 8 - 3
+ * >> answer = 2
+ * 
+*/
+
+import java.util.Stack;
+
+public class InfixEvaluation {
+    public static void main(String[] args) {
+    }
+}
